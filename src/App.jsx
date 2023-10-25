3fcf6a827193eac1f5fb3909802386f646300888
mhnipu <mazedul.hoque@televersant.com>
2023-10-25 12:31:31 +0600

    4th day

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About'
 import Features from './components/Features'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <Features />
       <About />
+      <Pricing />
     </>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,63 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import about1 from '../assets/about1.png'
+import about2 from '../assets/about2.png'
 const About = () => {
     return (
-        <div className="md:px-14 p-4 max-w-screen mx-auto">
-            <div className="flex flex-col md:flex-row justify-between items-center gap-8 hover:translate-y-4 transition-all duration-500">
-                <div className="md:w-1/2 hover:scale-90 translate-y-4 transition-all duration-500">
-                    <img src={about1} alt="" />
+        <div className="md:px-14 px-4 max-w-screen mx-auto space-y-5 ">
+            <div>
+                <div className="flex flex-col md:flex-row justify-between items-center gap-8 hover:scale-90 translate-y-4 transition-all duration-500">
+                    <div className="md:w-1/2 hover:scale-90 translate-y-4 transition-all duration-500">
+                        <img src={about1} alt="" />
+                    </div>
+                    {/* about content */}
+                    <div className='md:w-2/5'>
+                        <h2 className="md:text-5xl text-3xl font-bold text-NeviDBlue mb-5 leading-normal ">We have been improving our product <span className='text-televersantBlue'>for many years.</span></h2>
+                        <p className='text-DarkGray text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
+                            'There are many different kinds of animals that live in China.</p>
+                        <button className='btn-primary'>Get Started</button>
+                    </div>
                 </div>
-                {/* about content */}
-                <div className='md:w-2/5'>
-                    <h2 className="md:text-5xl text-3xl font-bold text-NeviDBlue mb-5 leading-normal ">We have been improving our product <span className='text-televersantBlue'>for many years.</span></h2>
-                    <p className='text-DarkGray text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
-                        'There are many different kinds of animals that live in China.</p>
-                    <button className='btn-primary'>Get Started</button>
+                {/* 2nd part  */}
+                <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-10">
+                    <div className="md:w-1/2 hover:scale-90 translate-y-4 transition-all duration-500">
+                        <img src={about2} alt="" />
+                    </div>
+                    {/* about content */}
+                    <div className='md:w-2/5'>
+                        <h2 className="md:text-5xl text-3xl font-bold text-NeviDBlue mb-5 leading-normal ">You can Practice at any <span className='text-televersantBlue'>time convinent for you.</span></h2>
+                        <p className='text-DarkGray text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
+                            'There are many different kinds of animals that live in China.</p>
+                        <button className='btn-primary'>Get Started</button>
+                    </div>
+                </div><div className="flex flex-col md:flex-row-reverse justify-between items-center gap-8 hover:scale-90 translate-y-4 transition-all duration-500">
+                    <div className="md:w-1/2 hover:scale-90 translate-y-4 transition-all duration-500">
+                        <img src={about1} alt="" />
+                    </div>
+                    {/* about content */}
+                    <div className='md:w-2/5'>
+                        <h2 className="md:text-5xl text-3xl font-bold text-NeviDBlue mb-5 leading-normal ">We have been improving our product <span className='text-televersantBlue'>for many years.</span></h2>
+                        <p className='text-DarkGray text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
+                            'There are many different kinds of animals that live in China.</p>
+                        <button className='btn-primary'>Get Started</button>
+                    </div>
                 </div>
+                {/* 2nd part  */}
+                <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-10">
+                    <div className="md:w-1/2 hover:scale-90 translate-y-4 transition-all duration-500">
+                        <img src={about2} alt="" />
+                    </div>
+                    {/* about content */}
+                    <div className='md:w-2/5'>
+                        <h2 className="md:text-5xl text-3xl font-bold text-NeviDBlue mb-5 leading-normal ">You can Practice at any <span className='text-televersantBlue'>time convinent for you.</span></h2>
+                        <p className='text-DarkGray text-lg mb-7'>A good example of a paragraph contains a topic conclusion.
+                            'There are many different kinds of animals that live in China.</p>
+                        <button className='btn-primary'>Get Started</button>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     )
 }
